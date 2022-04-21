b625dc01d27098a8a9ba72b71e9a341785c4a7cd
RodrigoXVargas <rodrigox.vargas@alumnos.frm.utn.edu.ar>
2022-04-21 17:35:06 -0300

    agrego linea de class Empleado():

--- entidades/empleado.py
+++ entidades/empleado.py
@@ -1,33 +1,34 @@
-def __init__(self, nombre, apellido, contrasenia, correo):
-    self.__nombre = nombre
-    self.__apellido = apellido
-    self.__contrasenia = contrasenia
-    self.__correo = correo
+class Empleado():
+    def __init__(self, nombre, apellido, contrasenia, correo):
+        self.__nombre = nombre
+        self.__apellido = apellido
+        self.__contrasenia = contrasenia
+        self.__correo = correo
 
 
-#Getters
-def getNombre(self):
-    return self.__nombre
+    #Getters
+    def getNombre(self):
+        return self.__nombre
 
-def getApellido(self):
-    return self.__apellido
+    def getApellido(self):
+        return self.__apellido
 
-def getContrasenia(self):
-    return self.__contrasenia
+    def getContrasenia(self):
+        return self.__contrasenia
 
-def getCorreo(self):
-    return self.__correo
+    def getCorreo(self):
+        return self.__correo
 
 
-#Setter
-def setNombre(self, nombre):
-    self.__nombre = nombre
+    #Setter
+    def setNombre(self, nombre):
+        self.__nombre = nombre
 
-def setApellido(self, apellido):
-    self.__apellido = apellido
+    def setApellido(self, apellido):
+        self.__apellido = apellido
 
-def setContrasenia(self, contrasenia):
-    self.__contrasenia = contrasenia
+    def setContrasenia(self, contrasenia):
+        self.__contrasenia = contrasenia
 
-def setCorreo(self, correo):
-    self.__correo = correo
+    def setCorreo(self, correo):
+        self.__correo = correo
